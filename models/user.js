8e6403709158cc125956e5fb906e4ea11eb7f32b
Travis McGray <tmcgray204@gmail.com>
2025-11-10 21:27:02 -0500

    Added user model and signup route

--- models/user.js
+++ models/user.js
@@ -1,19 +1,15 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
-// const userSchema = new Schema({
-//     firstname: {
-//         type: String,
-//         default: ''
-//     },
-//     lastname: {
-//         type: String,
-//         default: ''
-//     },
-//     email: {
-//         type: Boolean,
-//         default: false
-//     }
-// });
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
 
-// module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const User = require('./models/user');
 
 const PORT = 3000;
+
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/territory-app')
     .then(() => console.log('Connected to MongoDB'))
@@ -14,6 +17,16 @@ app.get('/hello', (req, res) => {
 
 app.get('/status', (req, res) => {
     res.send('Server is running smoothly!');
+});
+
+app.post('/signup', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.create({ email, password });
+        res.status(201).json({ message: 'User created successfully', userId: user._id });
+    } catch (error) {
+        res.status(400).json({ message: 'Error creating user', error: error.message });
+    }
 });
 
 app.listen(PORT, () => {
